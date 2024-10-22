286f59e1e807b66635c460e1cfd768f85637a50c
srsahana <sahana562000@gmail.com>
2024-10-22 15:34:13 +0530

    added game structure

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,21 +1,27 @@
 import React from "react";
-import "./styles.css"
+import "./styles.css";
 
 export const HomePage: React.FC=()=>{
+    const boxes=document.querySelectorAll(".box");
+
+    const clickHandler=()=>{
+       
+    }
+
     return(
         <>
         <h2>Tic Tac Toe</h2>
         <div className="container">
             <div className="wrapper">
-                <div className="box">0</div>
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
+                <button className="box" onClick={clickHandler}>0</button>
+                <button className="box"></button>
+                <button className="box"></button>
+                <button className="box"></button>
+                <button className="box"></button>
+                <button className="box"></button>
+                <button className="box"></button>
+                <button className="box"></button>
+                <button className="box"></button>
             </div>
         </div>
         </>
