6bd30cd2575850310b96475066e023dc0a4d47a2
srsahana <sahana562000@gmail.com>
2024-11-07 21:34:42 +0530

    components created

---
+++ src/components/Boxes/Box.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+
+export const Box: React.FC=()=>{
+    return(
+        <div className="box"></div>
+    )
+}
--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,27 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
+import { Box } from "../Boxes/Box";
 
 export const HomePage: React.FC=()=>{
     const boxes=document.querySelectorAll(".box");
 
-    const clickHandler=()=>{
-       
-    }
+    const [state, setState]=useState(Array(9).fill(null));
 
     return(
         <>
         <h2>Tic Tac Toe</h2>
         <div className="container">
             <div className="wrapper">
-                <button className="box" onClick={clickHandler}>0</button>
-                <button className="box"></button>
-                <button className="box"></button>
-                <button className="box"></button>
-                <button className="box"></button>
-                <button className="box"></button>
-                <button className="box"></button>
-                <button className="box"></button>
-                <button className="box"></button>
+                <Box/>
+                <Box/>
+                <Box/>
+                <Box/>
+                <Box/>
+                <Box/>
+                <Box/>
+                <Box/>
+                <Box/>
             </div>
         </div>
         </>
