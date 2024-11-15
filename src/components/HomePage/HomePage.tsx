a47c7829135bacf9a232c1e3fc192d4de7fa930b
srsahana <sahana562000@gmail.com>
2024-11-15 19:26:14 +0530

    disabled the boxes after game finished

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./styles.css";
 import { Box } from "../Boxes/Box";
-import { isDisabled } from "@testing-library/user-event/dist/utils";
 
 export const HomePage: React.FC=()=>{
 
@@ -39,15 +38,15 @@ export const HomePage: React.FC=()=>{
                 {win && <h3>You Won!!</h3>}
         <div className="container">
             <div className="wrapper">
-                <Box value={state[0]} onClick={()=>handleOnClick(0)} isDisabled={state[0]!=null?true:false}/>
-                <Box value={state[1]} onClick={()=>handleOnClick(1)} isDisabled={state[1]!=null?true:false}/>
-                <Box value={state[2]} onClick={()=>handleOnClick(2)} isDisabled={state[2]!=null?true:false}/>
-                <Box value={state[3]} onClick={()=>handleOnClick(3)} isDisabled={state[3]!=null?true:false}/>
-                <Box value={state[4]} onClick={()=>handleOnClick(4)} isDisabled={state[4]!=null?true:false}/>
-                <Box value={state[5]} onClick={()=>handleOnClick(5)} isDisabled={state[5]!=null?true:false}/>
-                <Box value={state[6]} onClick={()=>handleOnClick(6)} isDisabled={state[6]!=null?true:false}/>
-                <Box value={state[7]} onClick={()=>handleOnClick(7)} isDisabled={state[7]!=null?true:false}/>
-                <Box value={state[8]} onClick={()=>handleOnClick(8)} isDisabled={state[8]!=null?true:false}/>
+                <Box value={state[0]} onClick={()=>handleOnClick(0)} isDisabled={state[0]!=null || win ?true:false}/>
+                <Box value={state[1]} onClick={()=>handleOnClick(1)} isDisabled={state[1]!=null || win ?true:false}/>
+                <Box value={state[2]} onClick={()=>handleOnClick(2)} isDisabled={state[2]!=null || win ?true:false}/>
+                <Box value={state[3]} onClick={()=>handleOnClick(3)} isDisabled={state[3]!=null || win ?true:false}/>
+                <Box value={state[4]} onClick={()=>handleOnClick(4)} isDisabled={state[4]!=null || win ?true:false}/>
+                <Box value={state[5]} onClick={()=>handleOnClick(5)} isDisabled={state[5]!=null || win ?true:false}/>
+                <Box value={state[6]} onClick={()=>handleOnClick(6)} isDisabled={state[6]!=null || win ?true:false}/>
+                <Box value={state[7]} onClick={()=>handleOnClick(7)} isDisabled={state[7]!=null || win ?true:false}/>
+                <Box value={state[8]} onClick={()=>handleOnClick(8)} isDisabled={state[8]!=null || win ?true:false}/>
             </div>
         </div>
         </>
